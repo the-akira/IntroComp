69881dcd5fe250992843b4b8650127c16c9861b4
Gabriel Felippe <gabrielfelippe90@gmail.com>
2020-06-03 00:22:05 -0300

    Adicionados programas: cat, input_data & pointers

---
+++ Exemplos_C/input_data.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int soma(int x, int y);
+
+// programa que lê dois inteiros 
+// e retorna sua respectiva soma
+int main(void)
+{
+	int x, y;
+	printf("Digite um valor para x: ");
+	if(scanf("%d", &x)){};
+	printf("Digite um valor para y: ");
+	if(scanf("%d", &y)){};
+	printf("A soma de x e y é = %d\n", soma(x, y));
+}
+
+int soma(int x, int y)
+{
+	int z;
+	z = x + y;
+	return z;
+}
---
+++ Exemplos_C/pointers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 1, y = 2, z[10];
+	int *ip; // ip é um ponteiro para int
+	int **id; // id é um ponteiro para um ponteiro int
+	printf("Endereço de x = %p \n", &x); // imprime o endereço de x
+	printf("Endereço de ip = %p \n", &ip); // imprime o endereço de ip
+	printf("Endereço de id = %p \n", &id); // imprime o endereço de id
+
+	ip = &x; // ip atualmente aponta para x
+	id = &ip; // id atualmente aponta par y
+	printf("Valor de ip = %d \n", *ip); // imprime o valor de ip
+	printf("Valor de id = %d \n", **id); // imprime o valor de id
+
+	y = *ip; // y é atualmente 1
+	printf("Valor de y = %d \n", y);
+
+	*ip = 0; // x é atualmente 0
+	printf("Novo valor de ip = %d \n", *ip); // imprime o valor de ip
+
+	ip = &z[0]; // ip atualmente aponta para z[0]
+	printf("Novo valor de ip = %d \n", *ip); // imprime o valor de ip
+}
