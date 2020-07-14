88333f38a2ea2ab006e72bcad1695deac7e9d63f
Gabriel Felippe <gabrielfelippe90@gmail.com>
2020-07-13 21:29:56 -0300

    Problema de Recursão adicionado: adding_up.py

---
+++ Exemplos Python/Recursion/adding_up.py
@@ -0,0 +1,15 @@
+"""
+Função recursiva que computa
+a soma dos elementos de uma lista
+até um determinado índice
+"""
+def adding_up(lst,index):
+	soma = lst[index]
+	if index == 0:
+		return soma
+	return soma + adding_up(lst,index-1)
+
+lista = [1,2,3,10,15,50]
+print(adding_up(lista,0))
+print(adding_up(lista,3))
+print(adding_up(lista,5))
