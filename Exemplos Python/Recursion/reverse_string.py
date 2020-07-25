bc7e200632c5e00ab15c2860c4758a92e8452698
Gabriel Felippe <gabrielfelippe90@gmail.com>
2020-07-25 18:54:57 -0300

    Problema de Recursão adicionado: reverse_string.py

---
+++ Exemplos Python/Recursion/reverse_string.py
@@ -0,0 +1,10 @@
+# Invertendo uma string de maneira recursiva
+def reverse_string(string):
+	if string == '':
+		return ''
+	else:
+		return string[len(string)-1] + reverse_string(string[0:len(string)-1])
+
+string = 'gabriel'
+string_invertida = reverse_string(string)
+print(string_invertida)
