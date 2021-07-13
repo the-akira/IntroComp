65fe2d489bd6f55f663dd593baacbd268080cd51
Gabriel Felippe <gabrielfelippe90@gmail.com>
2021-07-12 23:16:00 -0300

    Script adding_up.py atualizado

--- Exemplos Python/Recursion/adding_up.py
+++ Exemplos Python/Recursion/adding_up.py
@@ -4,10 +4,10 @@ a soma dos elementos de uma lista
 até um determinado índice
 """
 def adding_up(lst,index):
-	soma = lst[index]
-	if index == 0:
-		return soma
-	return soma + adding_up(lst,index-1)
+    soma = lst[index]
+    if index == 0:
+        return soma
+    return soma + adding_up(lst,index-1)
 
 lista = [1,2,3,10,15,50]
 print(adding_up(lista,0))
